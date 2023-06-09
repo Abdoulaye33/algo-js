d38b89215a9b60c72051ebbb1ecb21e550a7855a
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-06-09 11:01:04 +0200

    commit

---
+++ 03-arrays/Exercise_3.3.js
@@ -0,0 +1,11 @@
+const commun = [1, 3, 4, 5];
+const secret = [];
+
+for (let i = 0; i < commun.length; i++ ) {
+    secret.push(commun[i]);
+}
+console.log(secret);
+
+const secretShiny = [1, 2, 3, 4, 5];
+const divine = secretShiny.slice();
+console.log("Abdulaib33 Dans Afs Au Commencement", divine);
