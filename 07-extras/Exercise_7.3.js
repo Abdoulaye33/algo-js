5efa01345c84ee98f7ac67b042837b2e38ea21b5
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-06-13 14:43:06 +0200

    commit

--- 07-extras/Exercise_7.3.js
+++ 07-extras/Exercise_7.3.js
@@ -1,47 +1,24 @@
-const readlineSync = require("readline-sync")
+const readlineSync = require('readline-sync');
 
-console.log("Hello! Welcome to the Pizza Flavors Manager.")
-console.log("\n")
-console.log("Please choose your actions:")
-let exit = false, value
-let menu = []
+function findDivisors(num) {
+  const divisors = [];
 
-function printMenu() {
-	console.log("\n")
-	console.log("1 - List all the pizza flavors")
-	console.log("2 - Add a new pizza flavor")
-	console.log("3 - Remove a pizza flavor")
-	console.log("4 - Exit this program")
-	console.log("\n")
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      divisors.push(i);
+    }
+  }
+
+  return divisors;
 }
 
-function printFlavor(menu) {
-	if (menu.length != 0) {
-		for (let i = 0; i < menu.length; i++) {
-			console.log((i + 1) + " - " + menu[i])
-		}
-	} else console.log("\nNo pizza!\n")
-}
+// Ask the user for a positive integer
+const num = parseInt(readlineSync.question('Enter a positive integer: '));
 
-while (!exit) {
-	printMenu()
-	let value = parseInt(readlineSync.question("Enter your action's number: "))
-	while (value < 0 && value > 4) {
-		value = parseInt(readlineSync.question("Enter your action's number: "))
-	}
-	
-	if (value === 1) {
-		printFlavor(menu)
-	} else if (value === 2) {
-		value = readlineSync.question("New flavor: ")
-		menu.push(value)
-		printFlavor(menu)
-	} else if (value === 3) {
-		value = parseInt(readlineSync.question("Number of the flavor to delete: "))
-		menu.splice((value - 1), 1)
-		printFlavor(menu)
-	} else {
-		exit = !exit;
-	}
+// Find and display the divisors
+const divisors = findDivisors(num);
+if (divisors.length === 0) {
+  console.log('There are no divisors for the given number.');
+} else {
+  console.log('Divisors:', divisors.join(' '));
 }
-console.log("You leave the program!")
