d8f96b01b6b13415ddddcc0da11260c30d3d088f
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-06-13 15:38:19 +0200

    commit

--- intro/exercices_1.1.js
+++
@@ -1,4 +0,0 @@
-const readlineSync = require("readline-sync");
-
-const message = "Hello, world!";
-console.log(message);
--- intro/exercices_1.3.js
+++
@@ -1,4 +0,0 @@
-const readlineSync = require("readline-sync");
-
-let firstName = readlineSync.question('Enter your first name pls');
-console.log("Hello " + firstName);
