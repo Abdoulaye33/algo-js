ff87f070e640842ad8111d437c0e67e645f94ee5
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-06-14 08:58:43 +0200

    commit

--- 02-DOM/1.Introduction/script1.js
+++ 02-DOM/1.Introduction/script1.js
@@ -1,1 +1,15 @@
 const title = document.querySelector("title").innerHTML = "Modifying the DOM";
+
+const body = document.querySelector("body");
+
+for (const element of body.children) {
+	
+}
+console.log(body)
+body.style.backgroundColor = "rgb(" + randomNum(0, 255) + ", " + randomNum(0, 255) + ", " + randomNum(0, 255) + ")"
+
+console.log(body.style.backgroundColor)
+
+function randomNum(min, max) {
+	return parseInt(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min))
+}
