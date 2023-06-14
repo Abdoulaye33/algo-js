4fe27877deb8aba62792b14968f07dc293df5f89
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-06-14 10:32:42 +0200

    commit

--- 02-DOM/2.Selectors/script2.js
+++ 02-DOM/2.Selectors/script2.js
@@ -3,8 +3,15 @@ const elementImportant = document.getElementsByClassName("important");
 for (let i = 0; i < elementImportant.length; i++) {
     const element = elementImportant[i];
     element.setAttribute("title", "this is an important element");
-}
+};
 
 
 const imgImportant = document.querySelectorAll("img");
 
+    imgImportant.forEach(img => {
+    if (img.classList.contains("important")) {
+        img.style = "border: 20px dotted green;";
+    } else {
+        img.style.display = "none";
+    }
+    });
